7dccac2446067a125eb77f029d11c4d391040664
Tomasz Poskonka <tomekp33@gmail.com>
2020-01-30 18:24:48 +0100

    next commit

---
+++ src/main/java/com/example/CascadeApp.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import com.example.entity.Company;
+import com.example.entity.CompanyDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CascadeApp {
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+        conf.configure("hibernate.cfg.xml");
+        conf.addAnnotatedClass(Company.class);
+        conf.addAnnotatedClass(CompanyDetail.class);
+        SessionFactory factory = conf.buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        Company company = new Company("KGHM", 1000000000);
+        CompanyDetail detail = new CompanyDetail("Poland", 15000);
+        company.setCompanyDetail(detail);
+
+
+        session.beginTransaction();
+
+        // session.save(company);
+        session.persist(company);
+        session.getTransaction().commit();
+
+        factory.close();
+    }
+}
---
+++ src/main/java/com/example/CascadeRemoveApp.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import com.example.entity.Company;
+import com.example.entity.CompanyDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CascadeRemoveApp {
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+        conf.configure("hibernate.cfg.xml");
+        conf.addAnnotatedClass(Company.class);
+        conf.addAnnotatedClass(CompanyDetail.class);
+        SessionFactory factory = conf.buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+
+
+        session.beginTransaction();
+
+        Company company = session.get(Company.class, 9);
+
+        session.remove(company);
+
+        session.getTransaction().commit();
+
+        factory.close();
+    }
+}
